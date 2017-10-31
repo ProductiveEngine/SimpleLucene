f6906e58374214e8d941931f793f6ba6669d771a
nifragos <nifragos@gmail.com>
2017-10-31 16:53:35 +0200

    Initial

---
+++ SimpleLucene/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+//Copy from the tutorial
+//https://www.codeproject.com/Articles/320219/Lucene-Net-ultra-fast-search-for-MVC-or-WebForms
+namespace SimpleLucene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LuceneSearch.AddUpdateLuceneIndex(SampleDataRepository.GetAll());
+            List<SampleData> s = LuceneSearch.Search("USA", "Description").ToList();
+            Console.WriteLine(s.Count);
+            Console.ReadKey();
+        }
+        
+    }
+}
